d6268bbe7ab74f3a098a42216833068289e249d1
MohanRajS-Tech <mohanselvam87@gmail.com>
2025-04-27 19:32:33 +0530

    Date Select & Assertion commit

--- flightBooking/src/flightBooking/FlightWebsite.java
+++ flightBooking/src/flightBooking/FlightWebsite.java
@@ -7,8 +7,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+
+
 
 public class FlightWebsite {
+	@SuppressWarnings("deprecation")
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		System.out.println("Flight Website Project"+'\n');
@@ -47,19 +51,7 @@ public class FlightWebsite {
 		System.out.println("\n"+ "No of Adults is");
 		System.out.println(driver.findElement(By.id("divpaxinfo")).getText());
 	
-//DYNAMIC DROPDOWN - The dropdown is loaded only after the section is selected
-		driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
-		driver.findElement(By.xpath("//a[@value='COK']")).click();
-		Thread.sleep(1000);
-		driver.findElement(By.xpath("(//a[@value='GOI'])[2]")).click();
-		
-//PARENT TO CHILD RELATIONSHIP XPATH
-//ALTERNATE TO INDEXING
-		driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
-		driver.findElement(By.xpath("//a[@value='COK']")).click();
-		Thread.sleep(1000);
-		driver.findElement(By.xpath("//div[@id='ctl00_mainContent_ddl_destinationStation1_CTNR']//a[@value='GOI']")).click();
-		
+
 			
 		
 		
@@ -76,14 +68,50 @@ public class FlightWebsite {
 		    	option.click();
 		    	break;
 			}}
-*/
+
 		
 //HANDLING CHECKBOX
+		System.out.println(driver.findElement(By.id("ctl00_mainContent_chk_friendsandfamily")).isSelected());
+		Assert.assertFalse(driver.findElement(By.id("ctl00_mainContent_chk_friendsandfamily")).isSelected());
+		driver.findElement(By.id("ctl00_mainContent_chk_friendsandfamily")).click();
+		System.out.println(driver.findElement(By.id("ctl00_mainContent_chk_friendsandfamily")).isSelected());
+		Assert.assertTrue(driver.findElement(By.id("ctl00_mainContent_chk_friendsandfamily")).isSelected());
+		Thread.sleep(2000);
+//REGULAR EXPRESSION IN LOCATOR - CSS SELECTOR
+		driver.findElement(By.cssSelector("input[name*='SeniorCitizenDiscount']")).click();
+	//COUNT OF CHECK BOXES
+		System.out.println("\n" + driver.findElements(By.cssSelector("input[type='checkbox']")).size());
+		Assert.assertEquals(driver.findElements(By.cssSelector("input[type='checkbox']")).size(), 6 );		
+*/
+//DYNAMIC DROPDOWN - The dropdown is loaded only after the section is selected
+		driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
+		driver.findElement(By.xpath("//a[@value='COK']")).click();
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("(//a[@value='GOI'])[2]")).click();
+				
+		//PARENT TO CHILD RELATIONSHIP XPATH
+		//ALTERNATE TO INDEXING
+				//driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
+				//driver.findElement(By.xpath("//a[@value='COK']")).click();
+				//Thread.sleep(1000);
+				//driver.findElement(By.xpath("//div[@id='ctl00_mainContent_ddl_destinationStation1_CTNR']//a[@value='GOI']")).click();
+				
+//CALENDER DROPDOWN
+		driver.findElement(By.cssSelector(".ui-datepicker-current-day")).click();
 		
-		
-		
-		
-		
+//VALIDATING IF ENABLED OR DISABLED
+		System.out.println(driver.findElement(By.cssSelector("#Div1")).getDomAttribute("style")+"\n");
+		driver.findElement(By.cssSelector("#ctl00_mainContent_rbtnl_Trip_1")).click();
+		//System.out.println(driver.findElement(By.cssSelector("#ctl00_mainContent_view_date2")).isEnabled());
+		System.out.println(driver.findElement(By.cssSelector("#Div1")).getDomAttribute("style"));
+		if(driver.findElement(By.cssSelector("#Div1")).getDomAttribute("style").contains("1"))
+		{
+		System.out.println("\n"+"Is Enabled");
+		Assert.assertTrue(true);
+		}
+		else {
+			Assert.assertTrue(false);
+		}
 		
 		
 		
--- flightBooking/src/module-info.java
+++ flightBooking/src/module-info.java
@@ -8,4 +8,6 @@ module flightBooking {
 requires org.seleniumhq.selenium.api;
 	requires org.seleniumhq.selenium.chrome_driver;
 	requires org.seleniumhq.selenium.support;
+	requires jdk.compiler;
+	requires org.testng;
 }
